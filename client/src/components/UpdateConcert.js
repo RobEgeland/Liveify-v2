45f09a65db8128348cb35e3edce7896feecfd990
Robert Egeland <robegeland@yahoo.com>
2022-09-16 17:10:00 -0500

    added state to update concert

--- client/src/components/UpdateConcert.js
+++ client/src/components/UpdateConcert.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 
 const UpdateConcert = ({concerts, artists}) => {
     const { id } = useParams()
     const currentConcert = concerts.filter(concert => concert.id === parseInt(id))
     const concertArtists = currentConcert[0].artists
-   console.log(concertArtists[1])
+    const [updatedConcert, setUpdatedConcert] = useState({
+        name: currentConcert[0].name,
+        location: currentConcert[0].location,
+        review: currentConcert[0].review,
+        user_id: currentConcert[0].user.id,
+        artist_id: [currentConcert[0].artists]
+    })
+    console.log(updatedConcert.artist_id[0][1].name)
     function handleChange() {
 
     }
@@ -17,41 +25,54 @@ const UpdateConcert = ({concerts, artists}) => {
     function handleArtistChange() {
 
     }
-    console.log(currentConcert)
-    return (
-        <form className='concert-form' onSubmit={handleSubmit}>
+
+    const artistcards = concertArtists.map(artist => {
+        <select name='artist_id' value={artist.name} onChange={handleArtistChange}>
+        {artists.map((artist) => <option key={artist.id} value={artist.id} >{artist.name}</option>)}
+        </select>
+    })
+    console.log(artistcards)
+    console.log(concertArtists)
+    if (updatedConcert) {
+        return (
+            <form className='concert-form' onSubmit={handleSubmit}>
                 <h1>Add a Concert</h1>
                 <div >
                     <label htmlFor='name'>Concert Name</label>
                     <br/>
-                    <input id="name" name='name' type={"text"} value={currentConcert[0].name} onChange={handleChange} />
+                    <input id="name" name='name' type={"text"} value={updatedConcert.name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='location'>Location</label>
                     <br/>
-                    <input id='location' name='location' type={"text"} value={currentConcert[0].location} onChange={handleChange}/>
+                    <input id='location' name='location' type={"text"} value={updatedConcert.location} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor='review'>Review</label>
                     <br/>
-                    <textarea id='review' name='review' type={"text"} value={currentConcert[0].review} onChange={handleChange}/>
+                    <textarea id='review' name='review' type={"text"} value={updatedConcert.review} onChange={handleChange}/>
                 </div>
                 <div>
                     <label>Change Who you saw</label>
                     <br/>
-                    <select name='artist_id' value={concertArtists[0]} onChange={handleArtistChange}>
+                    {artistcards}
+                    {/* <select name='artist_id' value={updatedConcert.artist_id[0][0].name} onChange={handleArtistChange}>
                     {artists.map((artist) => <option key={artist.id} value={artist.id} >{artist.name}</option>)}
                     </select>
-                    <select name='artist_id' value={concertArtists[1]} onChange={handleArtistChange}>
+                    <select name='artist_id' value={updatedConcert.artist_id[0][1].name} onChange={handleArtistChange}>
                     {artists.map((artist) => <option key={artist.id} value={artist.id} >{artist.name}</option>)}
                     </select>
-                    <select name='artist_id' value={concertArtists[2]} onChange={handleArtistChange}>
+                    <select name='artist_id' value={updatedConcert.artist_id[0][2].name} onChange={handleArtistChange}>
                     {artists.map((artist) => <option key={artist.id} value={artist.id} >{artist.name}</option>)}
-                    </select>
+                    </select> */}
                 </div>
                 <input type={"submit"} value={"create concert"}/>
             </form>
     )
+    }else {
+        <h1>Loading...</h1>
+    }
+   
 }
 
 export default UpdateConcert
